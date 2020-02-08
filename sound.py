39f0342898b651a7aec086455de8948eabf36368
fiullax <mattialerda8@gmail.com>
2020-02-08 12:46:30 +0100

    Work in progress on first call of a track

--- sound.py
+++ sound.py
@@ -1,5 +1,6 @@
 import soundcloud
-import json
+#import json
+import webbrowser
 
 # Client_id retrived from kodi add-on
 key = 'cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
@@ -10,33 +11,17 @@ client_secret = '3332dcf51a6f9d2c659dbb57c8068ed'
 
 client = soundcloud.Client(client_id=key)
 
-# a permalink to a track
-track_url = 'https://soundcloud.com/mattias-fiullax-games/sets/trap-italiana-free'
+# a permalink to a track/playlist
+playlist_url = 'https://soundcloud.com/mattias-fiullax-games/sets/trap-italiana-free'
 
-# resolve track URL into track resource
-playlist = client.get('/resolve', url=track_url)
+# resolve track URL into  resource
+playlist = client.get('/resolve', url=playlist_url)
 
-data = json.loads(str(playlist))
-print data['two']
+print(playlist.obj['tracks'])
 
-"""
-for track in playlist.tracks:    
-    data = json.loads(track)
-    print(data)
+for track in playlist.obj['tracks']:
+    url = track['permalink_url']
     break
 
-# now that we have the track id, we can get a list of comments, for example
-for track in client.get('/tracks/%d/comments' % track.id):
-    print 'Someone said: %s at %d' % (comment.body, comment.timestamp)
-
-
-tracks = client.get('/tracks', limit=1)
-for track in tracks:
-    print (track.author + '|' + track.title)
-    
-    # get the tracks streaming URL
-    stream_url = client.get(track.stream_url, allow_redirects=False)
-
-    # print the tracks stream URL
-    print (stream_url.location)
-    """
+# Open url in a new window of the default browser, if possible
+webbrowser.open_new(url)
