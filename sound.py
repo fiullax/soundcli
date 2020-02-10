d170966af2be1c31d79fbba8d32d6b8a51343cfa
fiullax <mattialerda8@gmail.com>
2020-02-10 20:05:55 +0100

    First playlist track open via url on chrome

--- sound.py
+++ sound.py
@@ -1,6 +1,7 @@
 import soundcloud
-#import json
+import json
 import webbrowser
+import urllib.request
 
 # Client_id retrived from kodi add-on
 key = 'cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
@@ -20,8 +21,26 @@ playlist = client.get('/resolve', url=playlist_url)
 print(playlist.obj['tracks'])
 
 for track in playlist.obj['tracks']:
-    url = track['permalink_url']
+    track_id = track['id']
     break
 
+first_url = 'https://api-v2.soundcloud.com/tracks/TRACK_ID?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
+first_url = first_url.replace('TRACK_ID', str(track_id))
+
+print("First url: " + first_url)
+
+
+res = urllib.request.urlopen(first_url)
+data = json.loads(res.read().decode())
+
+progressive_url = data['media']['transcodings'][1]['url']
+
+progressive_url += '?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
+
+res_progressive_url = urllib.request.urlopen(progressive_url)
+data_1 = json.loads(res_progressive_url.read().decode())
+
+print(data_1['url'])
+
 # Open url in a new window of the default browser, if possible
-webbrowser.open_new(url)
+webbrowser.open_new(data_1['url'])
