3f1f6e8f6efc14b227413567df257afe1ad568df
Mattia Lerda <mattialerda@Mattias-MacBook.local>
2020-02-11 19:48:03 +0100

    Replaced module urllib with urllib2

--- sound.py
+++ sound.py
@@ -1,7 +1,7 @@
 import soundcloud
 import json
 import webbrowser
-import urllib.request
+import urllib2
 import vlc
 
 # Client_id retrived from kodi add-on
@@ -31,14 +31,14 @@ first_url = first_url.replace('TRACK_ID', str(track_id))
 print("First url: " + first_url)
 
 
-res = urllib.request.urlopen(first_url)
+res = urllib2.urlopen(first_url)
 data = json.loads(res.read().decode())
 
 progressive_url = data['media']['transcodings'][1]['url']
 
 progressive_url += '?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
 
-res_progressive_url = urllib.request.urlopen(progressive_url)
+res_progressive_url = urllib2.urlopen(progressive_url)
 data_1 = json.loads(res_progressive_url.read().decode())
 
 print(data_1['url'])
