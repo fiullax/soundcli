6aee58bf25c61796d9db91a7836272abf8c43e61
fiullax <mattialerda8@gmail.com>
2020-02-13 23:08:05 +0100

    First track played with SoundClient obj

--- sound.py
+++ sound.py
@@ -13,17 +13,59 @@ class SoundClient:
 	""" TODO: Secret code for log in user private area
 	client_secret = '3332dcf51a6f9d2c659dbb57c8068ed'
 	"""
+        vlc = None
+        player = None
 
 	def __init__(self):
 		self.client = soundcloud.Client(client_id=self.client_id)
+                self.vlc = vlc.Instance('--input-repeat=-1', '--fullscreen')
 
-	# resolve track URL into  resource
+        # FIXME: Review the description
+	# resolve track URL into resource
 	def resolve_url(self, url):
+                print('Resolving url: ' + url)
 		return self.client.get('/resolve', url=url)
 
-	# retrive a dict with the tracks of the playlist
+	# return a dict with the tracks of the playlist
 	def get_playlist_tracks(self, playlist):
+                print("Retriving tracks from playlist: " + playlist.title)
 		return playlist.obj['tracks']
+
+        # TODO: Review in general the function
+        # return the media url of the track (ready to be played)
+	def get_track_media_url(self, track_id):
+                print('Retriving media url for track #' + str(track_id))
+                track_url = 'https://api-v2.soundcloud.com/tracks/TRACK_ID?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
+                track_url = track_url.replace('TRACK_ID', str(track_id))
+
+                res = urllib2.urlopen(track_url)
+                data = json.loads(res.read().decode())
+
+                progressive_url = data['media']['transcodings'][1]['url']
+
+                progressive_url += '?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
+
+                res_progressive_url = urllib2.urlopen(progressive_url)
+                data_1 = json.loads(res_progressive_url.read().decode())
+
+                print('Media url: ' + data_1['url'])
+
+                return data_1['url']
+        
+        # Open a new VLC player
+        def open_player(self):
+            print('Instance a new VLC player..')
+            self.player = self.vlc.media_player_new() 
+        
+        # Define VLC media and play it
+        def play_media(self, media_url):
+            print('Playing track..')
+            media = self.vlc.media_new(media_url)
+
+            self.player.set_media(media)
+            self.player.play()
+
+            time.sleep(150)
 
 
 def main():
@@ -38,45 +80,13 @@ def main():
 		track_id = track['id']
 		break
 
-	first_url = 'https://api-v2.soundcloud.com/tracks/TRACK_ID?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
-	first_url = first_url.replace('TRACK_ID', str(track_id))
-
-	print("First url: " + first_url)
-
-
-	res = urllib2.urlopen(first_url)
-	data = json.loads(res.read().decode())
-
-	progressive_url = data['media']['transcodings'][1]['url']
-
-	progressive_url += '?client_id=cZQKaMjH39KNADF4y2aeFtVqNSpgoKVj'
-
-	res_progressive_url = urllib2.urlopen(progressive_url)
-	data_1 = json.loads(res_progressive_url.read().decode())
-
-	print(data_1['url'])
-
-	url = data_1['url']
-
+        media_url = sound_client.get_track_media_url(track_id)
+	
+        sound_client.open_player()
+        sound_client.play_media(media_url)
+        
 	# Open url in a new window of the default browser, if possible
 	# webbrowser.open_new(data_1['url'])
-
-	# define VLC instance
-	instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
-
-	# Define VLC player
-	player = instance.media_player_new()
-
-	# Define VLC media
-	media = instance.media_new(url)
-
-	# Set player media
-	player.set_media(media)
-
-	# Play the media
-	player.play()
-
-	time.sleep(150)
 
 
 if __name__ == "__main__":
