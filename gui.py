f954bd9dcaa01eece54177400493e0842bf47127
fiullax <mattialerda8@gmail.com>
2020-02-18 23:14:01 +0100

    Working on anchorLayout of kivy

--- gui.py
+++ gui.py
@@ -1,8 +1,27 @@
 from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.core.window import Window
 
-class TestApp(App):
+
+class Interface(AnchorLayout):
+
+    def __init__(self, **kwargs):
+        super(Interface, self).__init__(**kwargs)
+        anchor_lc = AnchorLayout(anchor_x='center', anchor_y='center')
+        lbl = Label(text="Am i a Label ?", size=(100, 100), size_hint=(None, None))
+        anchor_lc.add_widget(lbl)
+        self.add_widget(anchor_lc)
+        # red background color
+        Window.clearcolor = (1, 0, 0, 0)
+
+
+class SoundClient(App):
+
     def build(self):
-        return Button(text='Hello World')
+        return Interface()
 
-TestApp().run()
+
+if __name__ == '__main__':
+    SoundClient().run()
