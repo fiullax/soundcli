be29113a323d763a99e13bf1909acab38c5b1137
fiullax <mattialerda8@gmail.com>
2020-04-13 12:08:12 +0200

    Key input tests done

--- tests/hotkeys.py
+++ tests/hotkeys.py
@@ -10,14 +10,28 @@ COMBINATIONS = [
 # The currently active modifiers
 current = set()
 
-def execute():
-    print ("Soundkey hotkey!")
+def next_song():
+    print ("Soundkey next song!")
+
+def old_song():
+    print ("Soundkey old_song!")
+
+def pause_play():
+    print ("Soundkey pause_play!")
 
 def on_press(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
-        current.add(key)
-        if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            execute()
+    for COMBO in COMBINATIONS:
+        if key in COMBO:
+            current.add(key)
+            if all(k in current for k in COMBO):
+                if keyboard.Key.right in COMBO:
+                    next_song()
+                
+                if keyboard.Key.left in COMBO:
+                    old_song()
+
+                if keyboard.Key.down in COMBO:
+                    pause_play()
 
 def on_release(key):
     if any([key in COMBO for COMBO in COMBINATIONS]):
